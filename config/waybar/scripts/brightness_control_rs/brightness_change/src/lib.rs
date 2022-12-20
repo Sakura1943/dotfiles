5ede6aa94b95be8e230c17e0a376dfde989605f2
Sakura1943 <1436700265@qq.com>
2022-12-20 20:29:02 +0800

    update

---
+++ config/waybar/scripts/brightness_control_rs/brightness_change/src/lib.rs
@@ -0,0 +1,60 @@
+use std::sync::Arc;
+use brightness::blocking::Brightness as _;
+
+pub struct BrightnessChange;
+
+impl BrightnessChange {
+    pub fn with_value(value: u32) -> Result<(), brightness::Error> {
+        brightness::blocking::brightness_devices()
+            .try_for_each(move |device| {
+                let device = Arc::new(device.unwrap());
+                let name = device.device_name()?;
+                device.set(value)?;
+                println!("Device: {name} is {value}");
+                Ok(())
+            })
+    }
+    pub fn up_with_value(value: u32) -> Result<(), brightness::Error> {
+        brightness::blocking::brightness_devices()
+            .try_for_each(move |device|  {
+                let device = Arc::new(device.unwrap());
+                let name = device.device_name()?;
+                let old_value = device.get()?;
+                let new_value = old_value + value;
+                device.set(new_value)?;
+                println!("Device: {name} is {value}");
+                Ok(())
+            })
+    }
+    pub fn down_with_value(value: u32) -> Result<(), brightness::Error> {
+        brightness::blocking::brightness_devices()
+            .try_for_each(move |device| {
+                let device = Arc::new(device.unwrap());
+                let name = device.device_name()?;
+                let old_value = device.get()?;
+                let new_value = old_value - value;
+                device.set(new_value)?;
+                println!("Device: {name} is {value}");
+                Ok(())
+            })
+    }
+    pub fn get_value() -> Result<u32, brightness::Error> {
+        let value = brightness::blocking::brightness_devices().next().unwrap()?.get()?;
+        Ok(value) 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{thread::sleep, time::Duration};
+
+    #[tokio::test]
+    async fn it_works() {
+        assert!(BrightnessChange::with_value(50).is_ok());
+        sleep(Duration::from_secs(2));
+        assert!(BrightnessChange::up_with_value(10).is_ok());
+        sleep(Duration::from_secs(2));
+        assert!(BrightnessChange::down_with_value(10).is_ok());
+    }
+}
